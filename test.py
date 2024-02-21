5b6800a8c50c8368e3562f4c75cc2c89a5c3a238
JamesXiaoMO <jameswuxiaomo@gmail.com>
2024-02-22 00:18:31 +0900

    upload

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-import socket
+import time
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-s.connect(('192.168.0.101', 9213))
-s.sendall("wifi".encode('utf-8'))
-s.close()
+print(type(time.time()))
